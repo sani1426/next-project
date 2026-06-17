935fe0f13681a9e876052c73b81ab593ff669598
saman <sam.karimi727@gmail.com>
2026-06-17 23:25:28 +0330

    update layout.jsx

--- app/(main)/layout.jsx
+++ app/(main)/layout.jsx
@@ -4,6 +4,7 @@ import "../globals.css";
 const poppins = Poppins({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  weight:['100' , '200' , '300' , '400' , '500', '600','700','800','900']
 });
 
 
