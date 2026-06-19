7bd96607e169c583987823d242148f095258b369
saman <sam.karimi727@gmail.com>
2026-06-20 03:29:50 +0330

    update NavLink.jsx

--- components/UI/NavLink.jsx
+++ components/UI/NavLink.jsx
@@ -7,7 +7,7 @@ const NavLink = ({path , title}) => {
         return ( 
                 <Link href={path} className='flex flex-col items-center gap-1'>
                 <p className="hover:text-blue-500 transition-all duration-300">{title}</p>
-                <hr className={`w-2/4 border-none h-[1.5px] bg-gray-700 ${pathName == path ? "" : "hidden"}`} />
+                <hr className={`w-2/4 border-none transition-all duration-300 h-[1.5px] bg-gray-700 ${pathName == path ? "w-3/4" : "hidden"}`} />
               </Link>
          );
 }
