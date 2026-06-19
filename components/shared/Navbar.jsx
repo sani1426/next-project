a6e0bebd212474ff69c2097646a5abafc9c9a653
saman <sam.karimi727@gmail.com>
2026-06-20 02:43:33 +0330

    update Navbar.jsx

--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -1,22 +1,38 @@
-
 import Link from 'next/link'
 import { assets, navItems } from '../../assets/assets'
 import NavLink from '../UI/NavLink'
 
-
 const Navbar = () => {
-
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
       {/* <img src={assets.logo} alt="logo-image" className="w-36" /> */}
       <h1 className='text-3xl font-bold'>سامان شاپ</h1>
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
-      {
-        navItems.map((n) => (
-                <NavLink key={n.id} path={n.href} title={n.title} />
-        ))
-      }
+        {navItems.map((n) => (
+          <NavLink key={n.id} path={n.href} title={n.title} />
+        ))}
       </ul>
+      <div className='flex items-center gap-6'>
+        <img
+          src={assets.search_icon}
+          className='w-5 cursor-pointer'
+          alt='serarch-icon'
+        />
+        <div className='group relative:'>
+          <img
+            src={assets.profile_icon}
+            className='w-5 cursor-pointer'
+            alt=''
+          />
+          <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+            <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+              <p className='cursor-pointer hover:text-black'>پروفایل</p>
+              <p className='cursor-pointer hover:text-black'>سفارشات</p>
+              <p className='cursor-pointer hover:text-black'>خروج</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
