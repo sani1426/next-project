d1fbc246bd02511de3c74abe4c23aca306de160f
saman <sam.karimi727@gmail.com>
2026-06-20 03:49:17 +0330

    update globals.css and Navbar.jsx

--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -16,9 +16,9 @@ const Navbar = () => {
       </ul>
       <div className='flex items-center gap-6'>
      <FiSearch className='text-2xl cursor-pointer' />
-        <div className='group relative'>
+        <div className='gr relative'>
     <BsPersonCircle className='text-2xl cursor-pointer' />
-          <div className='group-hover:block hidden absolute dropdown-menu -left-2 pt-4'>
+          <div className='gr-h hidden absolute dropdown-menu -left-2 pt-4'>
             <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
               <p className='cursor-pointer hover:text-black'>پروفایل</p>
               <p className='cursor-pointer hover:text-black'>سفارشات</p>
