cc96c822ba4d0a74e08e4d4ac328040a7142f813
saman <sam.karimi727@gmail.com>
2026-06-20 03:38:26 +0330

    update NavLink.jsx and Navbar.jsx

--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium px-10'>
       {/* <img src={assets.logo} alt="logo-image" className="w-36" /> */}
-      <h1 className='text-3xl font-bold'>سامان شاپ</h1>
+      <h1 className='text-4xl font-bold'>سامان شاپ</h1>
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
         {navItems.map((n) => (
           <NavLink key={n.id} path={n.href} title={n.title} />
