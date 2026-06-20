33f25e4418b1aeac74aaaeece7a0777aa35e2cae
saman <sam.karimi727@gmail.com>
2026-06-21 01:15:37 +0330

    update Navbar.jsx

--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 import { assets, navItems } from '../../assets/assets'
 import NavLink from '../UI/NavLink'
 import { FiSearch } from 'react-icons/fi'
-import { BsPersonCircle } from 'react-icons/bs'
-import { FaCartShopping } from "react-icons/fa6";
+import { GoPerson } from "react-icons/go";
+import { BsHandbag } from "react-icons/bs";
 
 const Navbar = () => {
   return (
@@ -18,7 +18,7 @@ const Navbar = () => {
       <div className='flex items-center gap-6'>
         <FiSearch className='text-2xl cursor-pointer' />
         <div className='gr relative'>
-          <BsPersonCircle className='text-2xl cursor-pointer' />
+          <GoPerson className='text-2xl cursor-pointer' />
           <div className='gr-h opacity-0 pointer-events-none absolute transition-all duration-400 dropdown-menu left-0 pt-4'>
             <div className='flex flex-col gap-2   bg-slate-100 text-gray-500 rounded-md p-2'>
               <Link href="/" className='cursor-pointer text-hovering px-8 py-1'>پروفایل</Link>
@@ -28,8 +28,8 @@ const Navbar = () => {
           </div>
         </div>
         <Link href="/cart" className='relative'>
-          <FaCartShopping className='text-2xl cursor-pointer' />
-          <p className='absolute riht-[-5px] -bottom-1.25 w-4 text-center leading-4 bg-white border border-black text-black aspect-square rounded-full text-[8px]'>10</p>
+          <BsHandbag className='text-2xl cursor-pointer' />
+          <p className='absolute riht-[-5px] -bottom-1.25 w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
         </Link>
       </div>
     </div>
