fbe329a067c74392d64b4334fe0f699a38ac5c2a
saman <sam.karimi727@gmail.com>
2026-06-21 01:48:24 +0330

    update MobileMenu.jsx

--- components/shared/MobileMenu.jsx
+++ components/shared/MobileMenu.jsx
@@ -2,14 +2,20 @@
 
 import { useState } from 'react';
 import { FaAlignLeft } from 'react-icons/fa'
+import { IoIosArrowForward } from "react-icons/io";
 
 const MobileMenu = () => {
         const [visible , setVisible] = useState(false)
         return ( 
                 <>
                  <FaAlignLeft onClick={()=> setVisible(!visible)} className='text-2xl cursor-pointer sm:hidden' />
-                 <div className={`absolute top-0 left-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? "w-[40%]" : "w-0"}`}>
-
+                 <div className={`absolute top-0 left-0 bottom-0 overflow-hidden bg-white transition-all shadow-2xl ${visible ? "w-[40%]" : "w-0"}`}>
+                        <div className="flex flex-col text-gray-600">
+                                <div onClick={()=> setVisible(false)} className="flex items-center gap-4 p-3">
+                                        <IoIosArrowForward  className='text-2xl cursor-pointer'/>
+                                        <p>back</p>
+                                </div>
+                        </div>
                  </div>
                 </>
          );
