da20a36f6e0ccfd9050bdaed745c0df215a95305
saman <sam.karimi727@gmail.com>
2026-06-22 00:56:07 +0330

    update 1 file and create 1 file

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -1,5 +1,6 @@
 
 
+import BestSeller from "../../components/main/BestSeller";
 import Hero from "../../components/main/Hero";
 import LatestCollection from "../../components/main/LatestCollection";
 
@@ -8,6 +9,7 @@ export default function Home() {
 <div>
     <Hero />
     <LatestCollection />
+    <BestSeller />
 </div>
   );
 }
---
+++ components/main/BestSeller.jsx
@@ -0,0 +1,20 @@
+
+import { products } from "../../assets/assets";
+import ProductItems from "./ProductItem";
+const BestSeller = () => {
+        const BestSellerProduct = products.find((product) => product.bestseller == true)
+        return ( 
+                <div>
+                        {
+                                BestSellerProduct.map((item) => (
+                                        <ProductItems key={item._id} id={item._id} name={item.name} image={item.image} price={item.price} />
+                                ))
+                        }
+                </div>
+         );
+}
+ 
+export default BestSeller;
+<div>
+
+</div>
