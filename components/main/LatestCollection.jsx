a8fc7c02902773635421291236289b1111dd0762
saman <sam.karimi727@gmail.com>
2026-06-21 03:49:21 +0330

    update LatestCollection.jsx and ProductItem.jsx

--- components/main/LatestCollection.jsx
+++ components/main/LatestCollection.jsx
@@ -6,7 +6,7 @@ const LatestCollection = () => {
         latestProduct = products.slice(0,10)
         console.log(latestProduct);
         return ( 
-                <div className="my-10">
+                <div className="my-10 px-5  md:px-10">
                         <div className="text-center py-8 text-3xl">
                                 <Title text1={"جدیدترین"} text2={"کالکشن"} />
                                 <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600 ">جدیدترین محصولات روز دنیا و بروزترین کالکشن های مد روز</p>
@@ -14,7 +14,7 @@ const LatestCollection = () => {
                 <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
                         {
                                 latestProduct.map((p) =>(
-                                        <ProductItems key={p.id} product={p} />
+                                        <ProductItems key={p.id} id={p.id} name={p.name} image={p.image} price={p.price} />
                                 ))
                         }
                 </div>
--- components/main/ProductItem.jsx
+++ components/main/ProductItem.jsx
@@ -3,15 +3,15 @@
 import Link from 'next/link'
 import { useShopContext } from '../../context/ShopContext'
 
-const ProductItems = ({ product }) => {
+const ProductItems = ({ id , image , name , price }) => {
   const { currency } = useShopContext()
   return (
-  <Link className='text-gray-700 cursor-pointer shadow-hover' href={`/product/${product.id}`}>
+  <Link className='text-gray-700 cursor-pointer shadow-hover' href={`/product/${id}`}>
         <div className='overflow-hidden'>
-                <img className='hover:scale-110 transition ease-in-out' src={product.image[0]} alt="" />
+                <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt="" />
         </div>
-        <p className='pt-3 pb-1 text-sm'>{product.name}</p>
-        <p className='text-sm font-medium'>{currency}{product.price}</p>
+        <p className='pt-3 pb-1 text-sm'>{name}</p>
+        <p className='text-sm font-medium'>{currency}{price}</p>
   </Link>
   )
 }
