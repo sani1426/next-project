044120e74f64aea2ca8817fcebd0d4b0cd08c6cc
saman <sam.karimi727@gmail.com>
2026-06-22 00:20:54 +0330

    update ProductItem.jsx

--- components/main/ProductItem.jsx
+++ components/main/ProductItem.jsx
@@ -3,17 +3,32 @@
 import Link from 'next/link'
 import { useShopContext } from '../../context/ShopContext'
 import Image from 'next/image'
+import { useState } from 'react'
 
-const ProductItems = ({ id , image , name , price }) => {
+const ProductItems = ({ id, image, name, price }) => {
   const { currency } = useShopContext()
+  const [hover, setHover] = useState(false)
+  const Source = hover ? (image[1] ? image[1] : image[0]) : image[0]
   return (
-  <Link className='text-gray-700 cursor-pointer shadow shadow-hover rounded-md' href={`/products/${id}`}>
-        <div className='overflow-hidden'>
-                <Image className='hover:scale-110 transition ease-in-out' src={image[0]} alt="image" />
-        </div>
-        <p className='pt-3 pb-1 text-sm'>{name}</p>
-        <p className='text-sm font-medium'>{currency}{price}</p>
-  </Link>
+    <Link
+      className='text-gray-700 cursor-pointer shadow shadow-hover rounded-md'
+      href={`/products/${id}`}
+    >
+      <div className='overflow-hidden'>
+        <Image
+          onMouseOver={() => setHover(true)}
+          onMouseOut={() => setHover(false)}
+          className='hover:scale-110 transition ease-in-out'
+          src={Source}
+          alt='image'
+        />
+      </div>
+      <p className='pt-3 pb-1 text-sm'>{name}</p>
+      <p className='text-sm font-medium'>
+        {currency}
+        {price}
+      </p>
+    </Link>
   )
 }
 
