69ac5e4f528b7437da3ebe497e114281ce7e5c60
saman <sam.karimi727@gmail.com>
2026-06-22 02:54:32 +0330

    update Filters.jsx

--- components/collection/Filters.jsx
+++ components/collection/Filters.jsx
@@ -8,8 +8,11 @@ const Filters = () => {
         const [show , setShow] = useState(false)
         return ( 
                 <div className="min-w-60">
-                <p onClick={()=> setShow(!show)} className="my-2 text-xl flex items-center cursor-pointer gap-2">فیلتر ها</p>
-                <Image src={assets.dropdown_icon} alt="dropdown-icon" className={`h-3 sm:hidden ${show ? "rotate-90 " : ""}`} />
+                        <div className="flex flex-row gap-2">
+                        <p onClick={()=> setShow(!show)} className="my-2 text-xl flex items-center cursor-pointer gap-2">فیلتر ها</p>
+                <Image src={assets.dropdown_icon} alt="dropdown-icon" className={`h-3 sm:hidden ${show ? "-rotate-90 " : "rotate-180"}`} />
+                        </div>
+            
                 <div className={`border border-gray-300 pl-5 py-3 mt-6 ${show ? "" : "hidden"} sm:block`}>
                         <p className="mb-3 text-sm font-medium">دسته بندی ها</p>
                         <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
