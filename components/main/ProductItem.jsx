e3cbf87d9c5b7809045a6885da357b6cad0437f4
saman <sam.karimi727@gmail.com>
2026-06-21 03:55:51 +0330

    update ProductItem.jsx

--- components/main/ProductItem.jsx
+++ components/main/ProductItem.jsx
@@ -2,13 +2,14 @@
 
 import Link from 'next/link'
 import { useShopContext } from '../../context/ShopContext'
+import Image from 'next/image'
 
 const ProductItems = ({ id , image , name , price }) => {
   const { currency } = useShopContext()
   return (
-  <Link className='text-gray-700 cursor-pointer shadow-hover' href={`/product/${id}`}>
+  <Link className='text-gray-700 cursor-pointer shadow-hover' href={`/products/${id}`}>
         <div className='overflow-hidden'>
-                <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt="" />
+                <Image className='hover:scale-110 transition ease-in-out' src={image[0]} alt="image" />
         </div>
         <p className='pt-3 pb-1 text-sm'>{name}</p>
         <p className='text-sm font-medium'>{currency}{price}</p>
