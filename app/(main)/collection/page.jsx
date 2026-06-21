a0467dfc91e12865c29288a0736d308bcf0f9fb1
saman <sam.karimi727@gmail.com>
2026-06-22 03:11:15 +0330

    update page.jsx and Filters.jsx

--- app/(main)/collection/page.jsx
+++ app/(main)/collection/page.jsx
@@ -1,9 +1,30 @@
+import { products } from "../../../assets/assets";
 import Filters from "../../../components/collection/Filters";
+import ProductItems from "../../../components/main/ProductItem";
+import Title from "../../../components/main/Title";
 
 const Collection = () => {
         return ( 
-                <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t px-5  md:px-10">
+                <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t px-3  md:px-6">
                         <Filters />
+                        <div className="flex-1">
+                                <div className="flex justify-between text-base sm:text-2xl mb-4">
+                                        <Title text1={"همه ی"} text2={"محصولات"} />
+                                        <select className="border-2 border-gray-300 text-sm px-2">
+                                                <option value="relevent">مرتب سازی محصولات مرتبط</option>
+                                                <option value="low-high">ارزانترین به گرانترین</option>
+                                                <option value="high-low">گرانترین به ارزانترین</option>
+                                        </select>
+                                </div>
+
+                                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-y-6">
+                                        {
+                                                products.map((product) => (
+                                                        <ProductItems key={product._id} name={product.name} image={product.image} price={product.price} />
+                                                ))
+                                        }
+                                </div>
+                        </div>
                 </div>
          );
 }
--- components/collection/Filters.jsx
+++ components/collection/Filters.jsx
@@ -8,12 +8,12 @@ const Filters = () => {
         const [show , setShow] = useState(false)
         return ( 
                 <div className="min-w-60">
-                        <div className="flex flex-row gap-2">
+                        <div className="flex items-center flex-row gap-2">
                         <p onClick={()=> setShow(!show)} className="my-2 text-xl flex items-center cursor-pointer gap-2">فیلتر ها</p>
-                <Image src={assets.dropdown_icon} alt="dropdown-icon" className={`h-3 sm:hidden ${show ? "-rotate-90 " : "rotate-180"}`} />
+                <Image src={assets.dropdown_icon} alt="dropdown-icon" className={`h-3 sm:hidden ${show ? "rotate-90 " : "rotate-180"}`} />
                         </div>
             
-                <div className={`border border-gray-300 pl-5 py-3 mt-6 ${show ? "" : "hidden"} sm:block`}>
+                <div className={`border border-gray-300 pr-5 py-3 mt-6 ${show ? "" : "hidden"} sm:block`}>
                         <p className="mb-3 text-sm font-medium">دسته بندی ها</p>
                         <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
                                 <p className="flex gap-2">
@@ -28,7 +28,7 @@ const Filters = () => {
                         </div>
                 </div>
                         {/* subcategories */}
-                        <div className={`border border-gray-300 pl-5 py-3 my-5 ${show ? "" : "hidden"} sm:block`}>
+                        <div className={`border border-gray-300 pr-5 py-3 my-5 ${show ? "" : "hidden"} sm:block`}>
                         <p className="mb-3 text-sm font-medium">مدل ها</p>
                         <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
                                 <p className="flex gap-2">
