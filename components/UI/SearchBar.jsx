ffc35bed8aa68047ed233b827b9098bd054bafa1
saman <sam.karimi727@gmail.com>
2026-06-23 02:43:13 +0330

    update SearchBar.jsx

--- components/UI/SearchBar.jsx
+++ components/UI/SearchBar.jsx
@@ -10,7 +10,7 @@ const SearchBar = () => {
         const [search , setSearch] = useState('')
 
                 return  ( 
-                        <div className="border-t border-b bg-gray-50 text-center">
+                        <div className={`border-t border-b bg-gray-50 text-center ${showSearchBar ? '' : 'hidden'}`}>
                         <div className="inline-flex items-center border border-gray-400 px-5 py-2 my-5">
                                 <input value={search} onChange={(e)=>setSearch(e.target.value)} type="text" placeholder="سرچ کنید ..." className="flex-1 outline-none bg-inherit text-sm" />
                                 <Image src={assets.search_icon} alt="search-icon" className="w-4" />
