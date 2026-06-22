4543eda9db97f5d8d30cc39bad72cbd1bcde1f3b
saman <sam.karimi727@gmail.com>
2026-06-23 02:57:39 +0330

    update SearchBar.jsx

--- components/UI/SearchBar.jsx
+++ components/UI/SearchBar.jsx
@@ -1,14 +1,19 @@
 "use client"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useShopContext } from "../../context/ShopContext";
 import Image from "next/image";
 import { assets } from "../../assets/assets";
+import { usePathname } from "next/navigation";
 
 const SearchBar = () => {
         const {showSerachBar , setShowSearchBar} =useShopContext()
         const [search , setSearch] = useState('')
+        const pathName = usePathname()
 
+        useEffect(()=>{
+                setShowSearchBar(false)
+        },[pathName])
                 return showSerachBar ?  ( 
                         <div className='border-t border-b bg-gray-50 text-center'>
                         <div className="inline-flex items-center justify-center  border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
