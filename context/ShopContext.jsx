1169d99548d6c01d83c8c374a6270df58e85423f
saman <sam.karimi727@gmail.com>
2026-06-23 00:35:13 +0330

    update ShopContext.jsx

--- context/ShopContext.jsx
+++ context/ShopContext.jsx
@@ -1,27 +1,31 @@
-"use client"
+'use client'
 
-import { createContext, useContext, useState } from "react"
-import { products } from "../assets/assets"
+import { createContext, useContext, useState } from 'react'
+import { products } from '../assets/assets'
 
 export const ShopContext = createContext()
 
-export const ShopContextProvider = ({children}) => {
-        const currency = "$";
-        const delivery_fee = 10;
-        const [newProducts , setNewProducts] = useState(products)
-        const applyFilter = (category , subCategory) => {
-                setNewProducts(prev => {
-                        prev.filter(item => category.includes(item.category))
-                })
-                }
-              
-//        let  newProduct =   products.filter(item => item.category !== category)
-     
-        return(
-                <ShopContext.Provider value={{products, currency , delivery_fee,applyFilter,newProducts}}>
-                        {children}
-                </ShopContext.Provider>
-        )
+export const ShopContextProvider = ({ children }) => {
+  const currency = '$'
+  const delivery_fee = 10
+  const [newProducts, setNewProducts] = useState(products)
+  const applyFilter = (category, subCategory) => {
+    if (category.length > 0) {
+      setNewProducts((prev) => {
+        prev.filter((item) => category.includes(item.category))
+      })
+    }
+  }
+
+  //        let  newProduct =   products.filter(item => item.category !== category)
+
+  return (
+    <ShopContext.Provider
+      value={{ products, currency, delivery_fee, applyFilter, newProducts }}
+    >
+      {children}
+    </ShopContext.Provider>
+  )
 }
 
-export const useShopContext = () =>useContext(ShopContext)
+export const useShopContext = () => useContext(ShopContext)
