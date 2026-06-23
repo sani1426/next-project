41e6d3544e3ec321191d9603f38659115c6e59ca
saman <sam.karimi727@gmail.com>
2026-06-24 00:44:47 +0330

    update page.jsx

--- app/(main)/product/[id]/page.jsx
+++ app/(main)/product/[id]/page.jsx
@@ -5,6 +5,7 @@ const productPage = async ({params}) => {
         const {id} = await params
         let productData ;
       productData =  products.find(item => item._id == Number(id))
+      
   return (
     <div>
       <h1>your id is {productData.name}</h1>
