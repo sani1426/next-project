3ebcdeee37599a945e00c197c3d17abb7e5afb2f
saman <sam.karimi727@gmail.com>
2026-06-24 00:16:43 +0330

    update page.jsx

--- app/(main)/[id]/page.jsx
+++ app/(main)/[id]/page.jsx
@@ -1,15 +1,15 @@
-"use cache"
+"use client"
 
-import { Suspense } from "react";
 
-const ProductDetails = async ({params}) => {
-        const {id} = await params
+
+const ProductDetails =  ({params}) => {
+      
         return ( 
-                <Suspense fallback={<h1>loading</h1>}>
+           
                 <div>
-                        <h1>{id}</h1>
+                        <h1>{params.id}</h1>
                 </div>
-                </Suspense>
+     
          );
 }
  
